cc11613171f0c3ee2907c5243ab2a717399eccd8
mipunaruwan <nipunakalpa1234@gmail.com>
2022-09-07 10:02:49 +0530

    item table is created

--- Routes/item.js
+++ Routes/item.js
@@ -3,3 +3,19 @@ const mysql = require('mysql')
 const dbase = require('../configs/db.configs')
 const router = express.Router()
 const connection = mysql.createConnection(dbase.database)
+
+// item table 
+
+connection.connect(function (err) {
+if(err){
+    console.log(err);
+
+}else{
+    var itemTable = "CREATE TABLE IF NOT EXISTS item (code VARCHAR(15) PRIMARY KEY, description TEXT, qtyOnHand INT, unitPrice DOUBLE)" 
+    connection.query(itemTable, function (err, result) { 
+        if (result.warningCount === 0) {
+            console.log("table created");
+}
+})
+}
+})

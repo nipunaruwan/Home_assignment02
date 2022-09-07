99182ae0e8b8206c41e16f8527db558ed01601e4
mipunaruwan <nipunakalpa1234@gmail.com>
2022-09-07 16:26:39 +0530

    create order table  is completed

---
+++ Routes/order.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mysql = require('mysql')
+const dbase = require('../configs/db.configs')
+const router = express.Router()
+const connection = mysql.createConnection(dbase.database)
+
+
+// creating order table
+connection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        var orderTable = "CREATE TABLE IF NOT EXISTS orders (id VARCHAR(10  ) PRIMARY KEY, date DATE, cusId VARCHAR(6), CONSTRAINT FOREIGN KEY(cusId) REFERENCES customer(id) ON DELETE CASCADE ON UPDATE CASCADE)";
+        connection.query(orderTable, function (err, result) {
+            if (result.warningCount === 0) {
+                console.log("table created");
+            }
+        })
+    }
+})
